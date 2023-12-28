703e8f19543c777d5a0f787d53f9158c58ef2c96
icerain <616964638@qq.com>
2023-12-28 08:45:16 +0800

    feat: 欢迎页面添加动画切换效果

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,5 +1,5 @@
-import { defineComponent } from "vue";
-import { RouterView } from "vue-router";
+import { Transition, VNode, defineComponent } from "vue";
+import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
 import s from "./Welcome.module.scss"
 import mangosteem from "../assets/icons/mangosteen.svg"
 export const Welcome = defineComponent({
@@ -12,7 +12,19 @@ export const Welcome = defineComponent({
             <h1>山竹记账</h1>
           </header>
           <main class={s.main}>
-            <RouterView name="main"/>
+            <RouterView name="main">
+              {({Component:X,route:R}:{Component: VNode;route: RouteLocationNormalizedLoaded;}) => (
+                <Transition 
+                  name="slide-fade" 
+                  enterActiveClass={s["slide-fade-enter-active"]}
+                  leaveActiveClass={s["slide-fade-leave-active"]}
+                  enterFromClass={s["slide-fade-enter-from"]}
+                  leaveToClass={s["slide-fade-leave-to"]}
+                >
+                  {X}
+                </Transition>
+              )}
+            </RouterView>
           </main>
           <footer>
             <RouterView name="actions"/>
