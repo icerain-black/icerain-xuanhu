df09fc3e5922c47ec3cc422c1ea6f2c51f208462
icerain <616964638@qq.com>
2024-01-05 12:35:51 +0800

    wip:实现InputPad组件

--- src/components/Item/ItemCreate.tsx
+++ src/components/Item/ItemCreate.tsx
@@ -3,6 +3,7 @@ import s from "./ItemCreate.module.scss"
 import { MainLayout } from "../../shared/MainLayout/MainLayout";
 import { Icon } from "../../shared/Icon/Icon";
 import { Tab, Tabs } from "../../shared/Tabs/Tabs";
+import { InputPad } from "./InputPad";
 export const ItemCreate = defineComponent({
   setup(props, ctx) {
     return () => {
@@ -13,14 +14,19 @@ export const ItemCreate = defineComponent({
           icon:() => <Icon name="left" class={s.navIcon}></Icon>,
           title:() => "记一笔",
           default:() => (
-            <Tabs v-model:selected={ref_selected.value}>
-              <Tab kind="支出">
-                支出
-              </Tab>
-              <Tab kind="收入">
-                收入
-              </Tab>
-            </Tabs>
+            <>
+              <Tabs v-model:selected={ref_selected.value}>
+                <Tab kind="支出">
+                  支出
+                </Tab>
+                <Tab kind="收入">
+                  收入
+                </Tab>
+              </Tabs>
+              <div class={s.inputpad_wrapper}>
+                <InputPad></InputPad>
+              </div>
+            </>
           )
          }
         }</MainLayout>
