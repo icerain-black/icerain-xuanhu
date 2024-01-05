d44bcbaf6f3d91cd973262ab333d1850b1fe06c0
icerain <616964638@qq.com>
2024-01-05 13:04:50 +0800

    wip:实现InputPad组件部分样式

--- src/components/Item/InputPad.tsx
+++ src/components/Item/InputPad.tsx
@@ -1,45 +1,43 @@
-import {defineComponent} from "vue";
-import s from "./InputPad.module.scss"
+import { defineComponent } from "vue";
+import s from "./InputPad.module.scss";
 import { Icon } from "../../shared/Icon/Icon";
 export const InputPad = defineComponent({
   setup(props, ctx) {
     const buttons = [
-      {text:"1",onClick:() => {}},
-      {text:"2",onClick:() => {}},
-      {text:"3",onClick:() => {}},
-      {text:"清空",onClick:() => {}},
-      {text:"4",onClick:() => {}},
-      {text:"5",onClick:() => {}},
-      {text:"6",onClick:() => {}},
-      {text:"+",onClick:() => {}},
-      {text:"7",onClick:() => {}},
-      {text:"8",onClick:() => {}},
-      {text:"9",onClick:() => {}},
-      {text:"-",onClick:() => {}},
-      {text:".",onClick:() => {}},
-      {text:"0",onClick:() => {}},
-      {text:"删",onClick:() => {}},
-      {text:"提交",onClick:() => {}},
-    ]
+      { text: "1", onClick: () => {} },
+      { text: "2", onClick: () => {} },
+      { text: "3", onClick: () => {} },
+      { text: "清空", onClick: () => {} },
+      { text: "4", onClick: () => {} },
+      { text: "5", onClick: () => {} },
+      { text: "6", onClick: () => {} },
+      { text: "+", onClick: () => {} },
+      { text: "7", onClick: () => {} },
+      { text: "8", onClick: () => {} },
+      { text: "9", onClick: () => {} },
+      { text: "-", onClick: () => {} },
+      { text: ".", onClick: () => {} },
+      { text: "0", onClick: () => {} },
+      { text: "删", onClick: () => {} },
+      { text: "提交", onClick: () => {} },
+    ];
     return () => {
       return (
         <>
           <div class={s.info}>
-         <span class={s.date}>
-          <Icon name="menu"></Icon>
-          <span>
-            2020-10-10
-          </span>
-         </span>
-         <span class={s.amount}>
-          0
-         </span>
+            <span class={s.date}>
+              <Icon name="date" class={s.icon}></Icon>
+              <span>2020-10-10</span>
+            </span>
+            <span class={s.amount}>0</span>
           </div>
           <div class={s.buttons}>
-            {buttons.map(item => <button onClick={item.onClick}>{item.text}</button>)}
+            {buttons.map((item) => (
+              <button onClick={item.onClick}>{item.text}</button>
+            ))}
           </div>
         </>
-      )
-    }
+      );
+    };
   },
-})
+});
