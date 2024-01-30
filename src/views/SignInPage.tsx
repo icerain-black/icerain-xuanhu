219ff843631bbd3740e6a069f4f8fcc5c332e4b4
icerain <616964638@qq.com>
2024-01-30 21:11:17 +0800

    refactor: 删除登录时mock测试代码

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -47,11 +47,8 @@ export const SignInPage = defineComponent({
       ]))
 
       if (!hasError(errors)) {
-        const response = await http.post<{ jwt: string }>('/session', formData, {
-          params: { _mock: 'session' }
-        }).catch(onError)
-        console.log(response)
-        localStorage.setItem("jwt",response?.data.jwt)
+        let result = await http.post("/session",formData).catch(onError)
+        localStorage.setItem("jwt",result?.data.jwt)
         const returnTo = route.query.return_to?.toString()
         refreshMe()
         router.push(returnTo || "/")
