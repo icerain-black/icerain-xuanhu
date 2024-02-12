6cf113009a08ceaac1002129f92ffffcfc972479
icerain <616964638@qq.com>
2024-02-12 17:57:09 +0800

    feat: 实现删除标签功能

--- src/components/tags/TagEdit.tsx
+++ src/components/tags/TagEdit.tsx
@@ -4,10 +4,28 @@ import { MainLayout } from "../../shared/MainLayout/MainLayout";
 import { TagFrom } from "./TagForm";
 import { Button } from "../../shared/Button/Button";
 import { BackIcon } from "../../shared/BackIcon/BackIcon";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import { Dialog } from "vant";
+import { http } from "../../shared/http/http";
 export const TagEdit = defineComponent({
   setup(props, ctx) {
     const route = useRoute()
+    const router = useRouter()
+    const tagId = route.params.id
+
+    const onDeleteTag = async (options = {withItem:false}) => {
+      Dialog.confirm({
+        title: '警告',
+        message: options.withItem ? '请问是否要删除标签以及相关记账？' : '请问是否要删除标签？',
+      })
+        .then(async () => {
+        await http.delete(`/tags/${tagId}`,{
+          withItem:options.withItem.toString()
+        })
+        router.back()
+      })
+        .catch(() => {})
+    }
     return () => {
       return (
         <MainLayout>
@@ -16,14 +34,14 @@ export const TagEdit = defineComponent({
             icon: () => <BackIcon></BackIcon>,
             default: () => (
               <>
-                <TagFrom class={s.tagForm} id={+route.params.id}>
+                <TagFrom class={s.tagForm} id={+tagId}>
                   <div class={s.buttons}>
                     <div class={s.main}>
                       <Button class={[s.formItem, s.button]}>确定</Button>
                     </div>
                     <div class={s.remove}>
-                      <Button level="danger" class={s.remove_tag}>删除标签</Button>
-                      <Button level="danger" class={s.remove_tag_and_items}>删除标签和记账</Button>
+                      <Button type="button" level="danger" class={s.remove_tag} onClick={() => onDeleteTag()}>删除标签</Button>
+                      <Button type="button" level="danger" class={s.remove_tag_and_items} onClick={() => onDeleteTag({withItem:true})}>删除标签和记账</Button>
                     </div>
                   </div>
                 </TagFrom>
