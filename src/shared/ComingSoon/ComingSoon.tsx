95aee750d84445c7f218bb0ab3cb23991b1dbbf2
icerain <616964638@qq.com>
2024-02-24 15:35:09 +0800

    perf: 敬请期待页面添加返回按钮

--- src/shared/ComingSoon/ComingSoon.tsx
+++ src/shared/ComingSoon/ComingSoon.tsx
@@ -2,6 +2,8 @@ import { defineComponent, PropType } from 'vue'
 import { Center } from '../Center/Centet'
 import s from './ComingSoon.module.scss'
 import { Icon } from '../Icon/Icon'
+import { Button } from '../Button/Button'
+import { useRouter } from 'vue-router'
 export const ComingSoon = defineComponent({
   props: {
     name: {
@@ -9,12 +11,19 @@ export const ComingSoon = defineComponent({
     }
   },
   setup: () => {
+    const router = useRouter()
+    const onClick = () => {
+      router.back()
+    }
     return () => (
       <div>
         <Center class={s.pig_wrapper}>
           <Icon name="pig" class={s.pig} />
         </Center>
         <p class={s.text}>敬请期待</p>
+        <div class={s.link}>
+          <Button onClick={onClick}>返回</Button>
+        </div>
       </div>
     )
   }
